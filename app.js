8de6985e1d06e0820e073f67baa12e7aed527836
AlitalipSever <alitalipsever@gmail.com>
2020-12-19 04:59:27 +0300

    file getting with expressjs

--- app.js
+++ app.js
@@ -4,6 +4,22 @@ var path = require('path');     //path module for folder dir. getting
 var app = express();
 app.use('/public', express.static(path.join(__dirname,'public'))); // use this for getting public files
 
+var logoutController = function(request,response){                  // getting logout.html file
+    response.sendFile(path.join(__dirname,'logout.html'));
+}
+
+var flowerController = function(request, response){
+    response.sendFile(path.join(__dirname,'flower.html'));
+}
+
+var roseController = function(request, response){
+    response.sendFile(path.join(__dirname,'rose.html'));
+}
+
+var yaseminController = function(request, response){
+    response.sendFile(path.join(__dirname,'yasemin.html'));
+}
+
 app.get('/', function(request,response){                //express GET method
     fs.readFile('index.html', function(error,data){
         if(error){                                      // error find
@@ -19,10 +35,9 @@ app.get('/login',function(request, response){
     response.sendFile(path.join(__dirname,'login.html'));      // using express readFile same work
 });
 
-app.get('/logout', function(request,response){
-    fs.readFile('logout.html',function(error, data){
-        response.write(data); 
-    })
-})
+app.get('/logout', logoutController);          //modular coding
+app.get('/flower', flowerController);
+app.get('/rose',roseController);
+app.get('/yasemin',yaseminController); 
 
 app.listen(7676);                   // now, we use app.listen different from http
